f93bc5efe05d381b87501309782f1b28820977fd
Micael Lima <5789811+mica-eu@users.noreply.github.com>
2024-02-06 21:23:45 -0300

    chore: update response

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
+		w.Write([]byte("Hello, World 2!"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
